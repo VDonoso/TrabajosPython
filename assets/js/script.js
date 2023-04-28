1751e49021f238cc0467d2265cbcac74c9f79661
Vasti Donoso Arévalo <130393051+VDonoso@users.noreply.github.com>
2023-04-28 15:01:47 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
+
+var url = "https://digimon-api.vercel.app/api/digimon"
+
+var contenido = document.querySelector("#contenido")
+
+fetch(url)
+.then(response => response.json())
+.then(datos => {
+    console.log(datos)
+
+    for (item of datos){
+
+    contenido.innerHTML += `
+  
+    <div class="tarjeta" :hover>
+    <div class="card" style="width: 8rem;">
+    <img src="${item.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <p class="card-text">${item.level}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+    </div> 
+    `
+}
+})
+
+
 $(document).ready(function() {
   // URL de la API
   var url = "https://digimon-api.vercel.app/api/digimon";
 
-  // Elemento <ul> donde se mostrarán los datos
+  /* Elemento <ul> donde se mostrarán los datos*/
   var listaDatos = $("#lista-datos");
 
   // Petición a la API con jQuery
@@ -17,49 +46,4 @@ $(document).ready(function() {
   });
 });
 
-var url = "https://digimon-api.vercel.app/api/digimon"
-
-var contenido = document.querySelector("#contenido")
-
-fetch(url)
-.then(response => response.json())
-.then(datos => {
-    console.log(datos)
-
-    for (item of datos){
-
-    contenido.innerHTML += `
-    
-<div class="tarjeta">
-        <div class="card" style="width: 18rem;">
-        <img src="${item.img}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${item.name}</h5>
-          <p class="card-text">${item.level}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-    </div>    
-    `
-
-}
-
-})
-
-
-
-function searchDigimon() {
-    
-var searchTerm = searchInput.value.toLowerCase();
-    
-fetch('https://digimon-api.vercel.app/api/digimon')
-  .then(response => response.json())
-  .then(datos => {
-
-var filteredData = datos.filter(function (digimon) {
-  return digimon.name.toLowerCase().includes(searchTerm);
-            });
-
-tabla(filteredData);
-        });
-}
+/**/
