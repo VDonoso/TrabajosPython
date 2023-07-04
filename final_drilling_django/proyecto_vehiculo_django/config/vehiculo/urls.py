b3d4fef742721926471e07b3e2ce6e6e4ed9c105
Vasti Donoso Arévalo <130393051+VDonoso@users.noreply.github.com>
2023-07-04 14:40:07 -0400

    Delete final_drilling_django/proyecto_vehiculo_django directory

--- final_drilling_django/proyecto_vehiculo_django/config/vehiculo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import IndexPageView, vehiculoforms_view, menuView
-
-urlpatterns = [
-    path('', IndexPageView.as_view(), name='index'),
-    path('vehiculo/add', vehiculoforms_view, name='vehiculoforms' ),
-    path('menu/', menuView, name='Menu'),
-]
